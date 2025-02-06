93ff61d987b5787d8228a29b7c4f206419aa1e60
Joris Mathijssen <jorismathijssen@gmail.com>
2025-02-06 15:22:46 +0100

    Allow local cors

---
+++ Backend/src/main/java/com/jorismathijssen/changecalculatorapi/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.jorismathijssen.changecalculatorapi.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    //TODO: I'm aware that this is not for production but for the current use case of showing off my java/react skills locally, this is okay.
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // Allow frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow cookies/auth
+            }
+        };
+    }
+}
